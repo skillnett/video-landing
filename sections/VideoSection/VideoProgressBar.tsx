35c1d95aa94388d361ac924cbc7e167f7393c2cf
skillnett <41345799+skillnett@users.noreply.github.com>
2022-08-03 18:09:41 +0300

    updating progress bar

--- sections/VideoSection/VideoProgressBar.tsx
+++ sections/VideoSection/VideoProgressBar.tsx
@@ -1,8 +1,27 @@
 import React, { memo } from 'react';
-import { ProgressBar } from './styles';
+import { useTheme } from 'styled-components';
 
 const VideoProgressBar = memo(({ progress }: any) => {
-    return <ProgressBar $width={progress} />;
+    const theme: any = useTheme();
+
+    return (
+        <div
+            style={{
+                transition: '1s linear all',
+                position: 'absolute',
+                height: 4,
+                width: `${progress}%`,
+                backgroundImage: `linear-gradient(
+          135deg,
+          ${theme.colors.gradientPrimary} 0%,
+          ${theme.colors.gradientSecondary} 100%
+      )`,
+                zIndex: 1,
+                bottom: 6,
+                borderEndStartRadius: 10,
+            }}
+        />
+    );
 });
 
 export default VideoProgressBar;
